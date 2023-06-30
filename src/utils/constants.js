90e7a9d1e3d2462ef6c59b51e51e5a2ec672bc77
KarinaRakhimova <rahimova@list.ru>
2023-06-30 21:47:16 +0500

    fx2 server addres in constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 // export const BASE_URL_MY_SERVER = 'http://localhost:3000/';
-export const BASE_URL = 'https://api.krr-diploma.nomoredomains.rocks/';
+export const BASE_URL_MY_SERVER = 'https://api.krr-diploma.nomoredomains.rocks/';
 export const BASE_URL_MOVIE_SERVER = 'https://api.nomoreparties.co';
