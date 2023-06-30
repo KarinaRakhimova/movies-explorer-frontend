9ff5f6d453c7a97562acfa712eb6de904a3317c8
KarinaRakhimova <rahimova@list.ru>
2023-06-30 21:20:48 +0500

    fix(validation hook): email check

--- src/hooks/useValidation.js
+++ src/hooks/useValidation.js
@@ -1,13 +1,22 @@
 import { useState } from "react";
+import { isEmail } from "validator";
+
 export default function useValidation() {
+
   const [values, setValues] = useState({});
   const [errors, setErrors] = useState({});
   const [isValid, setIsValid] = useState(false);
+
   function handleChange(evt) {
     const { name, value } = evt.target;
+    if (name === "email" && !isEmail(value)) {
+      evt.target.setCustomValidity("Заполните по образцу mail@mail.ru");
+    } else {
+      evt.target.setCustomValidity("");
+    }
     setValues({ ...values, [name]: value });
     setErrors({ ...errors, [name]: evt.target.validationMessage });
     setIsValid(evt.target.closest("form").checkValidity());
   }
-  return {values, setValues, errors, isValid, handleChange};
+  return { values, setValues, errors, isValid, handleChange };
 }
