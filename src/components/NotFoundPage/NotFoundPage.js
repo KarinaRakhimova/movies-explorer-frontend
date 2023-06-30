559ef7a67d43efc5fbb0b3746574191c09e1ebf1
KarinaRakhimova <rahimova@list.ru>
2023-06-30 21:25:40 +0500

    style(notfound): clearcode

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,14 +1,21 @@
 import { useNavigate } from "react-router-dom";
 export default function NotFoundPage() {
   const navigate = useNavigate();
+
   function goBack() {
-    navigate(-1);
-}
-  return(
+    if (window.history.state && window.history.state.idx > 0) {
+      navigate(-1);
+    } else {
+      navigate("/movies", { replace: true });
+    }
+  }
+  return (
     <div className="notFoundPage">
-    <p className="notFoundPage__code">404</p>
-    <p className="notFoundPage__text" >Страница не найдена</p>
-    <button className="notFoundPage__link link" onClick={goBack} >Назад</button>
-  </div>
+      <p className="notFoundPage__code">404</p>
+      <p className="notFoundPage__text">Страница не найдена</p>
+      <button className="notFoundPage__link link" onClick={goBack}>
+        Назад
+      </button>
+    </div>
   );
 }
